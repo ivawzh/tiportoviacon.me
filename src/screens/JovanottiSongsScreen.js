20a35e51cf4f276cad2bf69db62f03369dd6819e
Matteo Manzinello <matteo@manzinello.com>
2018-07-07 20:04:01 +0200

    Bozza di list card

--- src/screens/JovanottiSongsScreen.js
+++ src/screens/JovanottiSongsScreen.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
+import GridList from '@material-ui/core/GridList';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 // Algolia
 import { InstantSearch, Hits } from 'react-instantsearch-dom';
@@ -19,6 +26,27 @@ const styles = theme => ({
     menu: {
         width: 200,
     },
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+        width: 500,
+        height: 450,
+    },
+    subheader: {
+        width: '100%',
+    },
+    card: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+    },
 });
 
 const JovanottiSearchBox = ({ currentRefinement, refine }) =>
@@ -35,7 +63,34 @@ const JovanottiSearchBox = ({ currentRefinement, refine }) =>
 const ConnectedJovanottiSearchBox = connectSearchBox(JovanottiSearchBox);
 
 function JovanottiCard({ hit }) {
-    return <div>{hit.name} ciao</div>;
+    return (
+        <div>
+            <Card className={styles.card}>
+                <CardMedia
+                    className={styles.media}
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="headline" component="h2">
+                        {hit.name}
+        </Typography>
+                    <Typography component="p">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                        across all continents except Antarctica
+        </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Share
+        </Button>
+                    <Button size="small" color="primary">
+                        Learn More
+        </Button>
+                </CardActions>
+            </Card>
+        </div>
+    );
 }
 
 class JovanottiSongsScreen extends React.Component {
@@ -50,7 +105,11 @@ class JovanottiSongsScreen extends React.Component {
 
                     <ConnectedJovanottiSearchBox />
 
-                    <Hits hitComponent={JovanottiCard} />
+                    <div className={styles.root}>
+                        <GridList cellHeight={160} className={styles.gridList} cols={3}>
+                            <Hits hitComponent={JovanottiCard} />
+                        </GridList>
+                    </div>
 
                 </InstantSearch>
             </div>
