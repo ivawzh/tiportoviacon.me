8ad2f15dd14c2685c503b1775e3d5433ab8003ee
Matteo Manzinello <matteo@manzinello.com>
2018-07-10 12:55:00 +0200

    Creato il component JovanottiSong

---
+++ src/components/JovanottiSong.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+
+import VideocamIcon from '@material-ui/icons/Videocam';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import SubjectIcon from '@material-ui/icons/Subject';
+
+const styles = {
+    card: {
+        display: 'flex',
+        width: '100%',
+        marginTop: 24
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    cover: {
+        width: 180,
+        height: 180,
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: 24,
+        paddingBottom: 24,
+    },
+    playIcon: {
+        height: 44,
+        width: 44,
+    },
+};
+
+class JovanottiSong extends React.Component {
+
+    render() {
+        return (
+            <Card style={styles.card}>
+                <div className="jovanotti-card-media">
+                    <CardMedia
+                        style={styles.cover}
+                        image="http://www.gossipitaliano.net/wp-content/uploads/2018/02/jovanotti.jpg"
+                        title="Live from space album cover"
+                    />
+                </div>
+                <div style={styles.details}>
+                    <CardContent style={styles.content}>
+                        <Typography variant="headline">{this.props.song.name}</Typography>
+                        <Typography variant="subheading" color="textSecondary">
+                            Lorenzo Jovanotti
+            </Typography>
+                    </CardContent>
+                    <div style={styles.controls}>
+                        <IconButton aria-label="Previous">
+                            <SubjectIcon />
+                        </IconButton>
+                        <IconButton aria-label="Play/pause">
+                            <MusicNoteIcon style={styles.playIcon} />
+                        </IconButton>
+                        <IconButton aria-label="Next">
+                            <VideocamIcon />
+                        </IconButton>
+                    </div>
+                </div>
+            </Card>
+        );
+    }
+
+}
+
+export default JovanottiSong;
--- src/screens/JovanottiSongsScreen.js
+++ src/screens/JovanottiSongsScreen.js
@@ -1,18 +1,11 @@
 import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-
-import VideocamIcon from '@material-ui/icons/Videocam';
-import MusicNoteIcon from '@material-ui/icons/MusicNote';
-import SubjectIcon from '@material-ui/icons/Subject';
 
 // Algolia
 import { InstantSearch, InfiniteHits, connectSearchBox, PoweredBy } from 'react-instantsearch-dom';
+
+import JovanottiSong from '../components/JovanottiSong';
 
 const styles = {
     container: {
@@ -25,32 +18,6 @@ const styles = {
         justifyContent: 'space-around',
         overflow: 'hidden',
         width: '100%'
-    },
-    card: {
-        display: 'flex',
-        width: '100%',
-        marginTop: 24
-    },
-    details: {
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    content: {
-        flex: '1 0 auto',
-    },
-    cover: {
-        width: 180,
-        height: 180,
-    },
-    controls: {
-        display: 'flex',
-        alignItems: 'center',
-        paddingLeft: 24,
-        paddingBottom: 24,
-    },
-    playIcon: {
-        height: 44,
-        width: 44,
     },
 };
 
@@ -68,34 +35,7 @@ const ConnectedJovanottiSearchBox = connectSearchBox(JovanottiSearchBox);
 
 function JovanottiCard({ hit }) {
     return (
-        <Card style={styles.card}>
-            <div className="jovanotti-card-media">
-                <CardMedia
-                    style={styles.cover}
-                    image="http://www.gossipitaliano.net/wp-content/uploads/2018/02/jovanotti.jpg"
-                    title="Live from space album cover"
-                />
-            </div>
-            <div style={styles.details}>
-                <CardContent style={styles.content}>
-                    <Typography variant="headline">{hit.name}</Typography>
-                    <Typography variant="subheading" color="textSecondary">
-                        Lorenzo Jovanotti
-            </Typography>
-                </CardContent>
-                <div style={styles.controls}>
-                    <IconButton aria-label="Previous">
-                        <SubjectIcon />
-                    </IconButton>
-                    <IconButton aria-label="Play/pause">
-                        <MusicNoteIcon style={styles.playIcon} />
-                    </IconButton>
-                    <IconButton aria-label="Next">
-                        <VideocamIcon />
-                    </IconButton>
-                </div>
-            </div>
-        </Card>
+        <JovanottiSong song={hit} />
     );
 }
 
